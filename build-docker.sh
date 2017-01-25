434608b10c06b637c7974b59bb19e0cefbfc6347
Gilang Pambudi <mail@gilankpam.com>
2017-01-25 16:37:45 +0700

    test

--- build-docker.sh
+++ build-docker.sh
@@ -6,7 +6,10 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 PUSH=gitlab.skyshi.com:5000/gilankpam/php-ci:${BRANCH}-${TAG}
 
 echo "build docker with tag ${BRANCH}-${TAG}"
+echo $CI_BUILD_REF
+echo $CI_BUILD_REF_NAME
+echo $CI_BUILD_REF_SLUG
 
-docker build -t php-ci .
-docker tag php-ci $PUSH
-docker push $PUSH
+# docker build -t php-ci .
+# docker tag php-ci $PUSH
+# docker push $PUSH

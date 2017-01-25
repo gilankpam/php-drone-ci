7fbd56517e5abb35077ef3dae56fc8e625f22072
Gilang Pambudi <mail@gilankpam.com>
2017-01-25 16:42:35 +0700

    test

--- build-docker.sh
+++ build-docker.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
 
 # Get tag from commit sha1
-TAG=$(git rev-parse --short HEAD)
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-PUSH=gitlab.skyshi.com:5000/gilankpam/php-ci:${BRANCH}-${TAG}
+TAG=${CI_BUILD_REF_SLUG}-${CI_BUILD_REF:0:8}
 
-echo "build docker with tag ${BRANCH}-${TAG}"
-echo $CI_BUILD_REF
-echo $CI_BUILD_REF_NAME
-echo $CI_BUILD_REF_SLUG
+echo "Build docker with tag ${TAG}"
 
 # docker build -t php-ci .
 # docker tag php-ci $PUSH

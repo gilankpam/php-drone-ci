77734cd1fc9af40d4697d925d9af3f1f8f1bba9d
Gilang Pambudi <mail@gilankpam.com>
2017-01-25 13:53:20 +0700

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/hello[/{name}]', function($request, $response) {
+    $name = $request->getAttribute('name');
+    if (!$name) {
+        $name = "world!";
+    }
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->run();

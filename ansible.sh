a6c7412d09a3d3571409f22f9c005057b436439f
Gilang Pambudi <mail@gilankpam.com>
2017-02-22 16:20:18 +0700

    syalala

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
-# Docker image tag to pull
+# Get version from btanch name and commit sha1
 TAG=${CI_BUILD_REF_SLUG}-${CI_BUILD_REF:0:8}
 
+# create host file 
+echo "${DEPLOY_HOST} ansible_user=${DEPLOY_USER} ansible_ssh_pass=${DEPLOY_PASS}" > hosts
+
+# Run playbook
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -e "TAG=${TAG}" -i hosts playbook.yml
--- build-docker.sh
+++ build-docker.sh
@@ -5,10 +5,6 @@ VERSION=${CI_BUILD_REF_SLUG}-${CI_BUILD_REF:0:8}
 
 echo "Build docker with tag ${VERSION}"
 
-# docker build -t "${DOCKER_REPO}" .
-# Update the latest version also
-# docker push ${DOCKER_REPO}
-# Update with tag
 docker build -t "${DOCKER_REPO}:${VERSION}" .
 docker push "${DOCKER_REPO}:${VERSION}"
 # docker rmi -f "${DOCKER_REPO}"

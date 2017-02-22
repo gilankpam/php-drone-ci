c42f66ca20a13b98c588dcdabc415e3341e84d37
Gilang Pambudi <mail@gilankpam.com>
2017-02-22 14:14:45 +0700

    bajigurr mik kurang titik

--- build-docker.sh
+++ build-docker.sh
@@ -5,15 +5,11 @@ VERSION=${CI_BUILD_REF_SLUG}-${CI_BUILD_REF:0:8}
 
 echo "Build docker with tag ${VERSION}"
 
-echo "test docker"
-docker version
-docker ps
-
 # docker build -t "${DOCKER_REPO}" .
 # Update the latest version also
 # docker push ${DOCKER_REPO}
 # Update with tag
-docker build -t "${DOCKER_REPO}:${VERSION}"
+docker build -t "${DOCKER_REPO}:${VERSION}" .
 docker push "${DOCKER_REPO}:${VERSION}"
 # docker rmi -f "${DOCKER_REPO}"
 docker rmi -f "${DOCKER_REPO}:${VERSION}"
